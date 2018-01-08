d9b84624f43bcd3282817aee483f2972f23d488c
caroline reid <carolinehreid@gmail.com>
2018-01-08 21:18:20 +0000

    populates FilmList with data

--- src/components/FilmReleasesBox.js
+++ src/components/FilmReleasesBox.js
@@ -7,7 +7,13 @@ class FilmReleasesBox extends React.Component {
     super(props)
     this.state = {
       data: [
-        { id:1, film_title:"Sausage Party"}
+        { id:1, film_title:"Sausage Party"},
+        { id:2, film_title:"Café Society"},
+        { id:3, film_title:"Morgan"},
+        { id:4, film_title:"The 9th Life of Louis Drax"},
+        { id:5, film_title:"Naam Hai Akira"},
+        { id:6, film_title:"Equity"},
+        { id:7, film_title:"Things to Come"}                    
       ]
     }
   }
