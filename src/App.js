f0421e8df65ac4015c8589b568fd8d6fdf89143e
caroline reid <carolinehreid@gmail.com>
2018-01-08 20:21:11 +0000

    adds FilmReleasesBox component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import FilmReleasesBox from './components/FilmReleasesBox';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-
+      <FilmReleasesBox />
     );
   }
 }
---
+++ src/components/FilmReleasesBox.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class FilmReleasesBox extends React.Component {
+  render() {
+    return (
+      <div className="film-releases-box">
+        Checking that FilmReleasesBox displays.
+      </div>
+    )
+  }
+}
+
+export default FilmReleasesBox;
